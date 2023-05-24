f0cb1a96ea23571a33dae26cd8d4b20ab51b40fd
aero <aerooneQ@yandex.ru>
2023-05-24 22:27:45 +0300

    Better cpp Procfiler locator

--- src/dotnet/Procfiler/src/Core/CppProcfiler/ICppProcfilerLocator.cs
+++ src/dotnet/Procfiler/src/Core/CppProcfiler/ICppProcfilerLocator.cs
@@ -1,3 +1,4 @@
+using Procfiler.Utils;
 using Procfiler.Utils.Container;
 
 namespace Procfiler.Core.CppProcfiler;
@@ -10,5 +11,32 @@ public interface ICppProcfilerLocator
 [AppComponent]
 public class CppProcfilerLocatorImpl : ICppProcfilerLocator
 {
-  public string FindCppProcfilerPath() => Path.Combine(Directory.GetCurrentDirectory(), "CppProcfiler.dll");
+  private readonly IProcfilerLogger myLogger;
+
+  
+  public CppProcfilerLocatorImpl(IProcfilerLogger logger)
+  {
+    myLogger = logger;
+  }
+
+  
+  public string FindCppProcfilerPath()
+  {
+    var procfilerAssemblyLocation = Path.GetDirectoryName(GetType().Assembly.Location);
+    if (procfilerAssemblyLocation is null)
+    {
+      myLogger.LogError("The Procfiler.dll has no path: {Path}", procfilerAssemblyLocation);
+      throw new FileNotFoundException();
+    }
+    
+    var path = Path.Combine(procfilerAssemblyLocation, "CppProcfiler.dll");
+    if (!File.Exists(path))
+    {
+      myLogger.LogError("The CppProcfiler.dll does not exist here: {Path}", path);
+      throw new FileNotFoundException();
+    }
+    
+    myLogger.LogInformation("The cpp Procfiler is located at {Path}", path);
+    return path;
+  }
 }
