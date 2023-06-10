0e70b159c8d1b5023ac7fbad7138f537a3596f4b
aero <aerooneQ@yandex.ru>
2023-06-10 23:01:50 +0300

    Small refactorings

--- src/dotnet/Procfiler/src/Core/Collector/IShadowStacks.cs
+++ src/dotnet/Procfiler/src/Core/Collector/IShadowStacks.cs
@@ -3,3 +3,13 @@ namespace Procfiler.Core.Collector;
 public interface IShadowStacks
 {
 }
+
+public class EmptyShadowStacks : IShadowStacks
+{
+  public static EmptyShadowStacks Instance { get; } = new();
+  
+
+  private EmptyShadowStacks()
+  {
+  }
+}
